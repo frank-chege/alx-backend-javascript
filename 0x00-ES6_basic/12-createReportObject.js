73bf0ba083c2f36e341b2c2bf8f98d5c4077921c
frankrazzih <francischege602@gmail.com>
2024-04-27 02:55:48 +0300

    js basics

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
     let obj = {
         allEmployees: employeesList,
         getNumberOfDepartments: function(){
-            return Object.keys(obj).length;
+            return Object.keys(this.allEmployees).length;
         }
     }
     return obj;
