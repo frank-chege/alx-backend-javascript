5d38cabe1d51511b35e7cf402832c73f531dbe65
frankrazzih <francischege602@gmail.com>
2024-04-30 23:27:01 +0300

    js basics

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signUpUser from "./4-user-promise"
+import uploadPhoto from "./5-photo-reject"
+export default function handleProfileSignup(firstName, lastName, fileName){
+    return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => {
+        return res.map((resp) => ({
+            status : resp.status,
+            value: (() => {
+                if (resp.status === 'fulfilled'){
+                    return resp.value;
+                }
+                else{
+                    return String(resp.reason);
+                }
+            })()
+        }));
+    });
+}
