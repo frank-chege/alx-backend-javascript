ab8bb42513c491b9eab0256f53649289929394bd
frankrazzih <francischege602@gmail.com>
2024-05-15 01:30:13 +0300

    js data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,7 @@
 export default function getListStudentIds(arr){
     let id_arr = [];
     if (Array.isArray(arr)){
-        for (let obj of arr){
-            id_arr.push(obj.id);
-        }
+        id_arr = arr.map(obj => obj.id)
         return id_arr;
     }
     else{
