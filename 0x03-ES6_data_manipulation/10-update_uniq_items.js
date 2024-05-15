496c2e71229b42fd321dc9e384b475a717481481
frankrazzih <francischege602@gmail.com>
2024-05-15 21:10:59 +0300

    js data manipulation

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map_arg){
-    if (!map_arg instanceof Map){
+    if (!(map_arg instanceof Map)){
         throw new Error('Cannot process');
     }
     for (let [key, value] of map_arg){
